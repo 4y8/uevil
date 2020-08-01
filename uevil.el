2e7cb8b2ddce836b96641d7fa33f09ee12aa5f36
Yul3n <yul3n.falx@protonmail.com>
2020-08-01 10:33:18 +0200

    Initial commit.

---
+++ uevil.el
@@ -0,0 +1,25 @@
+;;; uevil.el --- A minimal implementation of vi emulation in emacs
+;;
+;; Copyright (C) 2020 Yul3n
+;;
+;; Author: Yul3n <http://github/Yul3n>
+;; Maintainer: John Doe <yul3n.falx@protonmail.com>
+;; Created: August 01, 2020
+;; Modified: August 01, 2020
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/Yul3n/uevil
+;; Package-Requires: ((emacs 26.3) (cl-lib "0.5"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+
+
+
+(provide 'uevil)
+;;; uevil.el ends here
